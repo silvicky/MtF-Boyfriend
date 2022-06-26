b5d77403503cbabde7b7f53bf72ffec0b3dada3e
silvicky <vickyisnotaboy@gmail.com>
2022-06-26 10:17:27 +0800

    Allow the addition of more extras.

--- maker.cmd
+++ maker.cmd
@@ -12,8 +12,22 @@ echo: >> result.txt
 echo: >> result.txt
 for %%a in (.\main\%vol%\???.txt) do copy /b result.txt+%%a result.txt&&echo: >> result.txt&&echo: >> result.txt
 set /a vol+=1
-if %vol% == 5 goto branchmk
+if %vol% == 5 goto extra2mk
 goto volumemk
+:extra2mk
+set vol=1
+:extramk
+echo: >> result.txt
+echo: >> result.txt
+echo: >> result.txt
+copy /b result.txt+".\extra\%vol%\info.txt" result.txt
+echo: >> result.txt
+echo: >> result.txt
+echo: >> result.txt
+for %%a in (.\extra\%vol%\???.txt) do copy /b result.txt+%%a result.txt&&echo: >> result.txt&&echo: >> result.txt
+set /a vol+=1
+if %vol% == 5 goto branchmk
+goto extramk
 :branchmk
 echo: >> result.txt
 echo: >> result.txt
@@ -23,14 +37,6 @@ echo: >> result.txt
 echo: >> result.txt
 echo: >> result.txt
 for %%a in (.\branch\???.txt) do copy /b result.txt+%%a result.txt&&echo: >> result.txt&&echo: >> result.txt
-echo: >> result.txt
-echo: >> result.txt
-echo: >> result.txt
-copy /b result.txt+".\extra\info.txt" result.txt
-echo: >> result.txt
-echo: >> result.txt
-echo: >> result.txt
-for %%a in (.\extra\???.txt) do copy /b result.txt+%%a result.txt&&echo: >> result.txt&&echo: >> result.txt
 echo: >> result.txt
 echo: >> result.txt
 echo: >> result.txt
